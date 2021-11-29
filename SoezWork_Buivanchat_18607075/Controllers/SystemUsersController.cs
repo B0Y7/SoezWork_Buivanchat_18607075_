d7e14f597499e27290a1ea52703525bd8e3e4e9a
B0Y7 <buichat1122@gmail.com>
2021-11-29 23:37:53 +0700

    add những controller ql người dùng

---
+++ SoezWork_Buivanchat_18607075/Controllers/SystemUsersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoezWork_Buivanchat_18607075.Models;
+
+namespace SoezWork_Buivanchat_18607075.Controllers
+{
+    public class SystemUsersController : Controller
+    {
+        private Graduation_Project_Work_ManagementEntities db = new Graduation_Project_Work_ManagementEntities();
+
+        // GET: SystemUsers
+        public ActionResult Index()
+        {
+            return View(db.SystemUsers.ToList());
+        }
+
+        // GET: SystemUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SystemUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Status,UserCode,FirstName,LastName,FullName,DateOfBirth,Address,Mobile,Email,Sex,UserTypeId,LoginFailedCount,LoginFailedCount2,LoginFailedCount3,LastChangedPassword,BranchId,ProfileImage,PositionId")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SystemUsers.Add(systemUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Status,UserCode,FirstName,LastName,FullName,DateOfBirth,Address,Mobile,Email,Sex,UserTypeId,LoginFailedCount,LoginFailedCount2,LoginFailedCount3,LastChangedPassword,BranchId,ProfileImage,PositionId")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(systemUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            db.SystemUsers.Remove(systemUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
